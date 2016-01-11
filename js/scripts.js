a730fce7e0b47c222724c61588ecbcb65db18e3f
Ryan Brown and Taylor Pokoj <pokojt@gmail.com>
2016-01-11 14:34:24 -0800

    It will add hover function to terms.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+  $("#flash").mouseenter(function() {
+    $(".definition").toggle();
+    $("h3").toggle();
+  });
+});
+
+/*$(function() {
+  $("#flash").mouseenter(function() {
+    $(".definition").hide();
+    $("h3").show();
+  });
+});
+*/
