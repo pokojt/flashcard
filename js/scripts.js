c93e6bae112341540ece6bcae36ad5a064e674a3
Ryan Brown and Taylor Pokoj <browneryan87@gmail.com>
2016-01-11 16:06:05 -0800

    Make js and jquery work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
 $(function() {
+  $("h3").click(function() {
+    $(this).next().toggle();
+    $(this).toggle(".definition");
+  });
+});
+
+
+$(function() {
+  $(".definition").click(function() {
+    $(this).hide();
+    $("h3").show();
+  });
+});
+
+
+
+
+
+
+/*
+$(function() {
   $("#flash").mouseenter(function() {
-    $(".definition").toggle();
+    $(".definiton").toggle();
     $("h3").toggle();
   });
 });
 
-/*$(function() {
-  $("#flash").mouseenter(function() {
-    $(".definition").hide();
-    $("h3").show();
+term
+this.next.slidetoggle
+
+
+$(function() {
+  $(".change").mouseEnter(function() {
+  $(this).next().slideToggle;
   });
-});
-*/
+}); */
